88db4bf61fc147f4cf7b7099ce94ffc1ac3c90ed
Jason Liu <jasoneliu03@gmail.com>
2021-06-08 01:12:39 -0400

    Moved providers from index to _app for multipage support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import * as themes from "../themes";
-import { ThemeContext } from "../context";
+import { ThemeContext, TestContext, SettingsContext } from "../context";
 import "../fonts.css";
 
 // Global style
 const GlobalStyle = createGlobalStyle`
   html, body {
     background-color: ${(props) => props.theme.colors.background};
-    font-family: "Myosevka", monospace;
-    /* font-family: Consolas, Menlo, "Courier New", monospace; */
     height: 100%;
     margin: 0;
     padding: 0;
-
     scrollbar-width: thin;
     scrollbar-color: ${(props) => props.theme.colors.secondary} transparent;
+  }
+
+  html, body, button {
+    font-family: "Myosevka", monospace;
   }
 
   * {
@@ -32,15 +34,50 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // ThemeContext values
   const [theme, setTheme] = useState<themes.themeType>("dark");
 
+  // TestContext values
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [linkRestartTest, setLinkRestartTest] = useState(0);
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
+  // SettingsContext values
+  const [settings, setSettings] = useState({
+    text: { capitals: false, symbols: false, numbers: false },
+    mode: "words",
+    length: {
+      timed: "30",
+      words: "50",
+      quotes: "medium",
+      lyrics: "medium",
+      books: "medium",
+      code: "medium",
+    },
+  });
+
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <ThemeProvider theme={themes[theme]}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <Provider session={pageProps.session}>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themes[theme]}>
+          <GlobalStyle />
+          <SettingsContext.Provider value={{ settings, setSettings }}>
+            <TestContext.Provider
+              value={{
+                timerRunning,
+                setTimerRunning,
+                linkRestartTest,
+                setLinkRestartTest,
+                settingsOpen,
+                setSettingsOpen,
+              }}
+            >
+              <Component {...pageProps} />
+            </TestContext.Provider>
+          </SettingsContext.Provider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </Provider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,26 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 import Head from "../components/Head";
 import Navbar from "../components/Navbar";
 import TypingTest from "../components/TypingTest";
 import Keytip from "../components/Keytip";
-import { TestContext, SettingsContext } from "../context";
 
 // Home page: typing test app
-const Home = () => {
-  // TestContext values
-  const [timerRunning, setTimerRunning] = useState(false);
-  const [linkRestartTest, setLinkRestartTest] = useState(0);
-  const [settingsOpen, setSettingsOpen] = useState(false);
-
-  // SettingsContext values
-  const [settings, setSettings] = useState({
-    text: { capitals: false, symbols: false, numbers: false },
-    mode: "words",
-    length: {
-      timed: "30",
-      words: "50",
-      quotes: "medium",
-      lyrics: "medium",
-      books: "medium",
-      code: "medium",
-    },
-  });
-
+const HomePage = () => {
   return (
-    <SettingsContext.Provider value={{ settings, setSettings }}>
-      <TestContext.Provider
-        value={{
-          timerRunning,
-          setTimerRunning,
-          linkRestartTest,
-          setLinkRestartTest,
-          settingsOpen,
-          setSettingsOpen,
-        }}
-      >
-        <Head />
-        <AppContainer>
-          <Navbar />
-          <TypingTest />
-          <Footer>
-            <Keytip />
-          </Footer>
-        </AppContainer>
-      </TestContext.Provider>
-    </SettingsContext.Provider>
+    <>
+      <Head />
+      <AppContainer>
+        <Navbar includeSettings={true} />
+        <TypingTest />
+        <Footer>
+          <Keytip />
+        </Footer>
+      </AppContainer>
+    </>
   );
 };
 
-export default Home;
+export default HomePage;
 
 const AppContainer = styled.div`
   max-width: 65rem;
