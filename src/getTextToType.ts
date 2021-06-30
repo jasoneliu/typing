fcb85b0c1b8507412ebd4c96d0bd852b2b840703
jasoneliu <jasoneliu03@gmail.com>
2021-06-30 17:14:20 -0400

    Added punctuation generation in text

--- src/getTextToType.ts
+++ src/getTextToType.ts
@@ -1,8 +1,8 @@
 // Fetch random typing text
 
+// generates a random number of length 1 to 4
 const generateNumber = () => {
   let number = "";
-  // number of length 1 to 4
   const length = Math.floor(Math.random() * 4) + 1;
   for (let digitIdx = 0; digitIdx < length; digitIdx++) {
     number += Math.floor(Math.random() * 10);
@@ -10,6 +10,68 @@ const generateNumber = () => {
   return number;
 };
 
+const capitalizeWord = (word: string) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+// adds random punctuation to a single word
+// punctuation: .,?!;:"'-() caps
+const addPunctuationToWord = (text: string[], wordIdx: number) => {
+  const rand = Math.random() * 100;
+  switch (true) {
+    case rand < 40: // ending punctuation .?!
+      if (rand < 30) {
+        text[wordIdx] += "."; // . 30%
+      } else if (rand < 35) {
+        text[wordIdx] += "?"; // ? 5%
+      } else {
+        text[wordIdx] += "!"; // ! 5%
+      }
+      // capitalize next word
+      if (wordIdx < text.length - 1) {
+        text[wordIdx + 1] = capitalizeWord(text[wordIdx + 1]);
+      }
+      return text;
+    case rand < 75:
+      text[wordIdx] += ","; // , 35%
+      return text;
+    case rand < 80:
+      text[wordIdx] += ";"; // ; 5%
+      return text;
+    case rand < 85:
+      text[wordIdx] += ":"; // : 5%
+      return text;
+    case rand < 90:
+      text[wordIdx] = "'" + text[wordIdx] + "'"; // '' 5%
+      return text;
+    case rand < 95:
+      text[wordIdx] = '"' + text[wordIdx] + '"'; // "" 5%
+      return text;
+    case rand < 100:
+      text[wordIdx] = "(" + text[wordIdx] + ")"; // () 5%
+      return text;
+    default:
+      // no punctuation
+      return text;
+  }
+};
+
+// adds random punctuation (capitals and symbols) to text
+const addPunctuation = (text: string[]) => {
+  text[0] = capitalizeWord(text[0]); // capitalize first word
+  let prevPuncPosition = 2; // prevent punctuation within close proximity
+  for (let wordIdx = 1; wordIdx < text.length; wordIdx++) {
+    if (Math.random() < 0.3 && prevPuncPosition >= 2) {
+      addPunctuationToWord(text, wordIdx);
+      prevPuncPosition = 0;
+    } else {
+      prevPuncPosition++;
+    }
+  }
+  return text;
+};
+
+// fetches a given number of random words
 const getWords = (numWords: number, punctuation: boolean, numbers: boolean) => {
   return fetch("/text/words.json")
     .then((response) => response.json())
@@ -19,6 +81,7 @@ const getWords = (numWords: number, punctuation: boolean, numbers: boolean) => {
       }
       const text: string[] = [];
       const wordList: string[] = data["oxford3000"];
+      // add random numbers
       if (numbers) {
         let prevNumberPosition = 2; // prevent numbers within close proximity
         for (let wordIdx = 0; wordIdx < numWords; wordIdx++) {
@@ -39,6 +102,10 @@ const getWords = (numWords: number, punctuation: boolean, numbers: boolean) => {
           wordList.splice(randIdx, 1); // remove chosen word from list for uniqueness
         }
       }
+      // add random punctuation (capitals and symbols)
+      if (punctuation) {
+        addPunctuation(text);
+      }
       return text;
     })
     .catch((error) => {
@@ -54,6 +121,7 @@ interface IQuote {
   id: number;
 }
 
+// converts an integer length (number of characters) to quote length (short, medium, long)
 const numLenToQuoteLen = (length: number) => {
   if (length < 100) {
     return "short";
@@ -64,7 +132,8 @@ const numLenToQuoteLen = (length: number) => {
   return "long";
 };
 
-const getQuotes = (quoteLength: string) => {
+// fetches a quote of given length
+const getQuote = (quoteLength: string) => {
   return fetch("/text/quotes.json")
     .then((response) => response.json())
     .then((data) => {
@@ -85,6 +154,7 @@ const getQuotes = (quoteLength: string) => {
     });
 };
 
+// fetches text of given mode, length, and text settings
 const getTextToType = (
   mode: string,
   length: string,
@@ -98,7 +168,7 @@ const getTextToType = (
       // TODO: make data fetching faster for long timed test
       return getWords(parseInt(length) * (200 / 60), punctuation, numbers);
     case "quote":
-      return getQuotes(length);
+      return getQuote(length);
     default:
       console.log("Error: not a valid mode");
       return getWords(-1, punctuation, numbers);
