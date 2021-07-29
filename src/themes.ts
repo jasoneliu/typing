4816092cc18f93239efb7805edcda1260888ec36
jasoneliu <jasoneliu03@gmail.com>
2021-07-28 21:07:08 -0400

    Temporarily replaced light theme with sakura theme

--- src/themes.ts
+++ src/themes.ts
@@ -28,13 +28,24 @@ export const dark: ITheme = {
 };
 
 export const light: ITheme = {
+  // colors: {
+  //   background: "hsl(0, 100%, 100%)",
+  //   primary: "hsl(208, 19%, 7%)",
+  //   secondary: "hsl(210, 15%, 68%)",
+  //   accent: "hsla(179, 95%, 40%)",
+  //   error: "hsl(340, 95%, 48%)",
+  //   errorExtra: "hsl(340, 95%, 79%)",
+  // },
+
+  // temporary "sakura" theme
+  // TODO: Create menu for more than two themes
   colors: {
-    background: "hsl(0, 100%, 100%)",
-    primary: "hsl(208, 19%, 7%)",
-    secondary: "hsl(210, 15%, 68%)",
-    accent: "hsla(179, 95%, 40%)",
-    error: "hsl(340, 95%, 48%)",
-    errorExtra: "hsl(340, 95%, 79%)",
+    background: "hsl(350, 100%, 80%)",
+    primary: "hsl(350, 100%, 100%)",
+    secondary: "hsl(350, 100%, 90%)",
+    accent: "hsl(350, 75%, 25%)",
+    error: "hsl(350, 100%, 65%)",
+    errorExtra: "hsl(350, 100%, 72%)",
   },
 };
 
