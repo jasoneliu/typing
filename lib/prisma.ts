3d93364d7f160850ffe0f654a344c907f11eaf79
jasoneliu <jasoneliu03@gmail.com>
2021-08-15 03:51:32 -0400

    Fixed global typing for prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient;
-}
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
