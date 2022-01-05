e3f77aa2c7533c7c9223770491f76d79815161ee
Jason Liu <jasoneliu03@gmail.com>
2022-01-05 03:46:38 -0500

    Reverted light theme

--- src/themes.ts
+++ src/themes.ts
@@ -27,26 +27,26 @@ export const dark: ITheme = {
   },
 };
 
+// TODO: Create menu for more than two themes
 export const light: ITheme = {
-  // colors: {
-  //   background: "hsl(0, 100%, 100%)",
-  //   primary: "hsl(208, 19%, 7%)",
-  //   secondary: "hsl(210, 15%, 68%)",
-  //   accent: "hsla(179, 95%, 40%)",
-  //   error: "hsl(340, 95%, 48%)",
-  //   errorExtra: "hsl(340, 95%, 79%)",
-  // },
-
-  // temporary "sakura" theme
-  // TODO: Create menu for more than two themes
   colors: {
-    background: "hsl(350, 100%, 80%)",
-    primary: "hsl(350, 100%, 100%)",
-    secondary: "hsl(350, 100%, 90%)",
-    accent: "hsl(350, 75%, 25%)",
-    error: "hsl(350, 100%, 65%)",
-    errorExtra: "hsl(350, 100%, 72%)",
+    background: "hsl(0, 100%, 100%)",
+    primary: "hsl(208, 19%, 7%)",
+    secondary: "hsl(210, 15%, 68%)",
+    accent: "hsla(179, 95%, 40%)",
+    error: "hsl(340, 95%, 48%)",
+    errorExtra: "hsl(340, 95%, 79%)",
   },
+
+  // "sakura" theme
+  // colors: {
+  //   background: "hsl(350, 100%, 80%)",
+  //   primary: "hsl(350, 100%, 100%)",
+  //   secondary: "hsl(350, 100%, 90%)",
+  //   accent: "hsl(350, 75%, 25%)",
+  //   error: "hsl(350, 100%, 65%)",
+  //   errorExtra: "hsl(350, 100%, 72%)",
+  // },
 };
 
 export type themeType = "dark" | "light";
